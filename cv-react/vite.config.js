955598b150366280fbc34c6f70c0b87db061bf81
Jorge Almarales <jorgealmarales22@gmail.com>
2026-05-28 16:00:25 -0500

    ci: agregar workflow para despliegue automatizado en github pages

--- cv-react/vite.config.js
+++ cv-react/vite.config.js
@@ -4,5 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Reemplaza 'cv-react' por el nombre exacto de tu repositorio en GitHub si es diferente
+  base: '/cv_jorge_almarales/', 
   plugins: [react(), tailwindcss()],
 })
